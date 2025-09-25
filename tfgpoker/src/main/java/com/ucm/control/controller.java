75648a568530242b0a434bc11339f2b508230c95
oscarFPG <95655329+oscarFPG@users.noreply.github.com>
2025-09-25 18:30:59 +0200

    Todos los cambios incluidos

---
+++ tfgpoker/src/main/java/com/ucm/control/controller.java
@@ -0,0 +1,19 @@
+package com.ucm.control;
+
+public class Controller{
+    
+    private Game _game;
+    
+    Controller(Game game){
+        _game = game;
+    }
+    
+    private void configureGame(){
+        
+    }
+    
+    public void run(){
+        
+    }
+    
+}
---
+++ tfgpoker/src/main/java/com/ucm/logic/game.java
@@ -0,0 +1,57 @@
+package com.ucm.logic;
+
+
+public class Game{
+    
+    public static int NUM_MIN_PLAYERS = 2;
+    public static int NUM_MAX_PLAYERS = 9;
+    
+    private Table _table;
+    
+    
+    public Game(){
+        _table = new Table();
+    }
+  
+    
+    public void addPlayerToTable(Player p){
+        _table.addPlayerToTable(p);
+    }
+    
+    public void shareOutAllCards(){
+        _table.shareOutAllCards();
+    }
+    
+    public void retrieveAllCards(){
+        _table.retrieveAllCards();
+    }
+    
+    public void addCardToTable(){
+        _table.addCardToTable();
+    }
+    
+    public void retrieveCardsFromTable(){
+        _table.retrieveCardsFromTable();
+    }
+    
+    public void assignRolesToAllPlayers(){
+        _table.assignRolesToAllPlayers();
+    }
+    
+    public void passTurn(){
+        _table.passTurn();
+    }
+    
+    public void playHand(){
+        _table.playFullHand();
+    }
+
+    public void printGame(){
+        _table.printTable();
+    }
+    
+    public boolean isGameFinished(){
+        return false;
+    }
+    
+}
