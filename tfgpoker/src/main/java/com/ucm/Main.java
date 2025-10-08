62b9717892535a979a21698bce8afbb57ceb0a48
oscar <oscarf1030p@gmail.com>
2025-10-08 20:23:18 +0200

    Ahora sí, debug listo

--- tfgpoker/src/main/java/com/ucm/Main.java
+++ tfgpoker/src/main/java/com/ucm/Main.java
@@ -25,11 +25,10 @@ public class Main extends Application {
     /*
      * Debug: 
      *  Primero: 
-     *      Ejecutar el comando:    cd tfgpoker ; .\mvnwDebug.cmd clean compile exec:java
+     *      Ejecutar el comando:    cd tfgpoker ; .\mvnwDebug.cmd exec:java
      *      Debe aparecer el mensaje: "Listening for transport dt_socket at address: 8000"
-     *  Segundo: 
-     *      cd tfgpoker ; .\mvnw.cmd clean
-     *      Darle a la tarea "Debug JavaFX project" en el apartado "Run and Debug" de VSCode
+     *  Segundo:
+     *      Darle a la tarea "Debug Poker project" en el apartado "Run and Debug" de VSCode
      */
     public static void main(String[] args) {
         
