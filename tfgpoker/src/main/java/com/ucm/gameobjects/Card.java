fa943b2fdeac76a61f049791dd555553b6c2ed19
oscar <oscarf1030p@gmail.com>
2025-10-21 13:29:28 +0200

    Finalizacion de ronda terminado: normal y al retirarse todos los jugadores

--- tfgpoker/src/main/java/com/ucm/gameobjects/Card.java
+++ tfgpoker/src/main/java/com/ucm/gameobjects/Card.java
@@ -7,33 +7,24 @@ public class Card {
     private boolean _available;
     private boolean _sol;
             
-    public Card (char number ,Suit suit) {
-        this._number = number;
-        this._suit = suit;
-        this._sol = false;
-        this._available = true;
+    public Card (char number, Suit suit) {
+        _number = number;
+        _suit = suit;
+        _sol = false;
+        _available = true;
     } 
     
-    public char getNumber(){
-        return _number;
-    }
-    
-    public Suit getSuit(){
-        return _suit;
-    }
     
     public void setSol(boolean b){
         _sol = b;
     }
+
     public void setAvailable(boolean b){
         _available = b;
     }
-  
-    public boolean getSol(){
-        return _sol;
-    }
-    
-     public boolean getAvailable(){
-        return _available;
-    }
+
+    public char getNumber(){ return _number; }
+    public Suit getSuit(){ return _suit; }
+    public boolean getSol(){ return _sol; }
+    public boolean getAvailable(){ return _available; }
 }
--- tfgpoker/src/main/java/com/ucm/middleclasses/CommandResult.java
+++ tfgpoker/src/main/java/com/ucm/middleclasses/CommandResult.java
@@ -1,13 +1,13 @@
 package com.ucm.middleclasses;
 
 
-public record CommandResult(int bet, boolean raises) {
+public record CommandResult(int bet, boolean raises, boolean folds) {
 
     public static CommandResult continuePlaying(int money, boolean raise) {
-        return new CommandResult(money, raise);
+        return new CommandResult(money, raise, false);
     }
 
     public static CommandResult stopPlaying(int money) {
-        return new CommandResult(money, false);
+        return new CommandResult(money, false, true);
     }
 }
