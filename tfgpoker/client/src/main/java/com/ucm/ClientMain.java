3929c86145fd2d35070521ffb4f1d37de8deb90b
Valeria <valeriacorinapl@gmail.com>
2025-11-18 22:05:44 +0100

    CLiente aun no acabdo

--- tfgpoker/client/src/main/java/com/ucm/ClientMain.java
+++ tfgpoker/client/src/main/java/com/ucm/ClientMain.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.util.Arrays;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
@@ -16,6 +17,8 @@ public class ClientMain extends Application {
     
     public static String host = "localhost";
     public static int port = 5005;
+    private static int Cards[] = new int[4];
+    private static int Board_Cards[] = new int[10];
 
     /*
      * Desde la ruta TFGPOKER/tfgpoker
@@ -52,9 +55,95 @@ public class ClientMain extends Application {
             // Mensaje no unido a partida
             else if ( conection == GameType.CONECTION_DECLINE){
                 System.out.print("Servidor ha rechazado la conexion") ;
+                return;
             }
 
+            boolean endOfGame = false;
+            while (!endOfGame){
+                // PRE- FLOP
+                //Repartir dos cartas
+                //Primero Number luego Suit
+                
+                try {
+                    // Repartir dos cartas
+                    for (int i = 0; i < 4; i++) {
+                        Cards[i] = SocketUtils.receiveInt(input);
+                    }
+                    System.out.println("Cartas recibidas: " + Arrays.toString(Cards));
+                } catch (IOException e) {
+                    System.out.println("Error al recibir cartas: " + e.getMessage());
+                }
+
+
+               
+                boolean endOfHand = false;
+                
+                //NO ES SHOW DOWN
+                while ( !endOfHand){
+                     boolean endOfRound = false;
+                    //RONDA DE APUESTAS, HASTA QUE NO ACABE, NO SE REPARTEN LAS SIGUIENTES CARTAS EN LA MESA
+                    while( !endOfRound){
+                        int action = SocketUtils.receiveInt(input);
+                        //AVISAR TURNO DE JUGADOR
+                        switch (action) {
+                            case GameType.TURN_WAIT:
+                                System.out.println("Usuario esperando a que sea su turno");
+                                break;
+                            case GameType.TURN_PLAY:
+                                //ACCION DEL JUGADOR
+                                System.out.println("Usuario a jugado: FOLD");
+                                SocketUtils.sendInteger(output, GameType.FOLD);
+                                break;
+                            case GameType.END_OF_ROUND:
+                                System.out.println("Fin de la ronda de apuestas");
+                                endOfRound = true;
+                                break;
+                            case GameType.END_OF_HAND: //ES SHOW_DOWN
+                                System.out.println("Fin de la mano");
+                                endOfRound = true;
+                                endOfHand= true;
+                                break;
+                            case GameType.END_OF_GAME:
+                                System.out.println("El juego ha acabado");
+                                endOfRound = true;
+                                endOfHand= true;
+                                endOfGame = true;
+                                break;
+                        }
+                    }
+                
+                    //REPARTIR CARTAS DE LA MESA
+                    if ( !endOfHand && !endOfGame){
+                        int fase = SocketUtils.receiveInt(input);
+                        int actual_board_cards = (fase == GameType.PRE_FLOP) ? 6 : 2;
+                        for (int i = 0; i < actual_board_cards; i++) {
+                            Board_Cards[i] = SocketUtils.receiveInt(input);
+                        }
+                        System.out.println("Cartas en la mesa recibidas: " + Arrays.toString(Board_Cards));
+                    }
+                }
+                
+
+                if (!endOfGame && endOfHand){
+                    int hand_winner = SocketUtils.receiveInt(input);
+                    if (hand_winner == GameType.HAND_OVER_WIN){
+                        System.out.println("Usuario ha ganado esta mano");
+                    }
+                    else if (hand_winner == GameType.HAND_OVER_LOSE){
+                        System.out.println("Usuario ha perdido esta mano");
+                    }
+                }
+
+            }
+
+            int game_winner = SocketUtils.receiveInt(input);
+            if (game_winner == GameType.GAME_OVER_WIN){
+                System.out.println("Usuario ha ganado el juego!");
+            }
+            else if (game_winner == GameType.GAME_OVER_LOSE){
+                System.out.println("Usuario ha perdido el juego");
             
+            }
 
             socket.close();
         }
@@ -63,6 +152,13 @@ public class ClientMain extends Application {
         }
         
     }
+
+
+    private void recieveCards(){
+
+
+    }
+
 
     @Override
     public void start(Stage stage) throws Exception {
