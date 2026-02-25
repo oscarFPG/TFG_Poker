61447e4dc4dcfe2b04ff4924bc36c4b7770b7e8a
CarlaTTaipe25 <carlatoa@ucm.es>
2026-02-25 19:49:40 +0100

    interfaz2

--- tfgpoker/src/main/java/com/ucm/App.java
+++ tfgpoker/src/main/java/com/ucm/App.java
@@ -1,17 +1,17 @@
 public class App extends Application{
 
     @Override
-
-    public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass(),getResource("templateStyle"))
+    public void start(Stage templateStyle) throws Exception {
+        Parent root = FXMLLoader.load(getClass(),getResource("templateStyle.fxml"))
         Scene scene = new Scene();
-        scene.getStylesheets().add(getClass().getResources("/styles/templateStyle.css"))
+        scene.getStylesheets().add(getClass().getResources("/styles/templateStyle.css").toExternalForm());
 
-        
+        templateStyle.setTitle("Venta Plantilla Estilo");
+        templateStyle.setScene(scene);
+        templateStyle.show();
     }
 
     public static void main(String[] args){
         launch(args);
     }
-
 }
