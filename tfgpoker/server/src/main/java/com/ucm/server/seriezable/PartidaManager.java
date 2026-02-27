30bd5d269c26318c45700a312e5497b19bf4a692
Valeria <valeriacorinapl@gmail.com>
2026-02-27 17:01:27 +0100

    Objeto Partida incompleto

---
+++ tfgpoker/server/src/main/java/com/ucm/server/seriezable/PartidaManager.java
@@ -0,0 +1,22 @@
+package com.ucm.server.seriezable;
+
+import java.io.FileInputStream;
+import java.io.ObjectInputStream;
+
+public class PartidaManager {
+
+
+    //convierte partida a doc
+    public static void savePartida(Partida partida, String ruta) throws Exception {
+        
+    }
+
+    //cara partida de un doc
+     public static Partida loadPartida(String ruta) throws Exception {
+        ObjectInputStream par = new ObjectInputStream(
+                new FileInputStream(ruta));
+        Partida partida = (Partida) par.readObject();
+        par.close();
+        return partida;
+    }
+}
