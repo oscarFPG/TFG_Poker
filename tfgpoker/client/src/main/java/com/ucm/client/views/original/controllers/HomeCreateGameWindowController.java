16b91ef8f106abdd340faa48ad32f51c1db6537b
CarlaTTaipe25 <carlatoa@ucm.es>
2026-04-05 14:16:56 +0200

    update variables de home

--- tfgpoker/client/src/main/java/com/ucm/client/views/original/controllers/HomeCreateGameWindowController.java
+++ tfgpoker/client/src/main/java/com/ucm/client/views/original/controllers/HomeCreateGameWindowController.java
@@ -8,18 +8,75 @@ import com.ucm.common.PokerGame;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 
 public class HomeCreateGameWindowController extends GenericController {
 
     @FXML
     private Button btnBackChooseGame;
+
     @FXML
     private Button btnNextHome;
+
     @FXML
     private Button btnSaveHome;
+
     @FXML
     private TextField textFieldRoomName;
+
+    @FXML
+    private ComboBox<String> comboBlindsValue;
+
+    @FXML
+    private ComboBox<String> comboLevelDuration;
+
+        @FXML
+    private ComboBox<String> comboHikePercentage;
+
+    @FXML
+    private void initialize(){
+        initializeBlindsValue();
+        initializeLevelDuration();
+        initializeHikePercentage();
+    }
+
+    private void initializeBlindsValue(){
+        comboBlindsValue.getItems().add("1/2");
+        int[] blinds = {5, 10, 25, 50, 100};
+        String defaultValue = "DEFAULT";
+        for (int blind : blinds){
+            comboBlindsValue.getItems().add(blind + "/" + (blind * 2));
+        }
+        comboBlindsValue.getItems().add(defaultValue);
+        comboBlindsValue.setValue(defaultValue);
+    }
+
+    private void initializeLevelDuration(){
+        String defaultValue = "DEFAULT";
+        comboLevelDuration.getItems().addAll(
+            "15",
+            "20",
+            "30",
+            "60",
+            "120",
+            "180"
+        );
+        comboLevelDuration.setValue(defaultValue);
+    }
+
+    private void initializeHikePercentage(){
+        String defaultValue = "DEFAULT";
+        comboHikePercentage.getItems().addAll(
+            "25",
+            "33",
+            "50",
+            "67",
+            "80",
+            "100"
+        );
+        comboHikePercentage.setValue(defaultValue);
+    }
 
     @FXML
     public void returnChooseGame(){
@@ -32,17 +89,21 @@ public class HomeCreateGameWindowController extends GenericController {
     }
 
     @FXML
-    public void saveHomeConfig(){
+    public void saveHomeConfig() {
+
         String roomName = textFieldRoomName.getText();
-        if(GameConfig.isValidRoomName(roomName)){
+
+
+        if(GameConfig.isValidRoomName(roomName)) {
             _clientInfo.gameConfig._roomName = roomName;
             try {
                 PokerGame.sendGameConfig(_clientInfo.gameConfig, _clientInfo.socket.getOutputStream());
-            } catch (IOException e) {
+            }
+            catch (IOException e) {
                 System.out.println( String.format("Error server room name %s\n", e.getMessage()) );
             }
         }
-        else{
+        else {
             if(GameConfig.checkRoomName(roomName) == GameType.ERROR_NAME_TOO_SHORT){
                 System.out.println("Name too short, try again");
             }
--- tfgpoker/common/src/main/java/com/ucm/common/GameConfig.java
+++ tfgpoker/common/src/main/java/com/ucm/common/GameConfig.java
@@ -6,7 +6,7 @@ public class GameConfig {
      * Variables para crear partida: Home
     */
     public String _roomName;
-    public String _roomId;
+    public int _roomId;
     public int _initialMoney;
     public boolean _allowBots;
     
