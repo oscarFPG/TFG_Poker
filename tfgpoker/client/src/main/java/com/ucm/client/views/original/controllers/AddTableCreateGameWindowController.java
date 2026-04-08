1e90630979ae6427c47a357865cce60c56d12a1a
CarlaTTaipe25 <carlatoa@ucm.es>
2026-04-09 00:33:17 +0200

    update add tables

--- tfgpoker/client/src/main/java/com/ucm/client/views/original/controllers/AddTableCreateGameWindowController.java
+++ tfgpoker/client/src/main/java/com/ucm/client/views/original/controllers/AddTableCreateGameWindowController.java
@@ -1,9 +1,13 @@
 package com.ucm.client.views.original.controllers;
 
+import java.util.List;
+
 import com.ucm.client.ClientInfo;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 public class AddTableCreateGameWindowController extends GenericController {
 
@@ -17,9 +21,47 @@ public class AddTableCreateGameWindowController extends GenericController {
     private Button btnStartAddTables;
 
     @FXML
-    private void initialize() {
+    private Button btnNextImageTable;
+
+    @FXML
+    private Button btnBackImageTable;
+
+    @FXML
+    private ImageView imageTable;
+
+    private List<Image> _images;
+
+    private int _index = 0;
+
+    @Override
+    protected void onViewShown() {
         btnStartAddTables.setDisable(true);
+        initializeImage();
     }
+
+    private void initializeImage() {
+        _images = List.of(
+            new Image(getClass().getResource("/images/tableGame.png").toExternalForm()),
+            new Image(getClass().getResource("/images/tableGameBlue.png").toExternalForm()),
+            new Image(getClass().getResource("/images/tableGameGreen.png").toExternalForm()),
+            new Image(getClass().getResource("/images/tableGameRed.png").toExternalForm())
+        );
+        imageTable.setImage(_images.get(_index));
+    }
+
+    @FXML
+    private void nextImage() {
+        _index = (_index + 1) % _images.size();
+        imageTable.setImage(_images.get(_index));
+    }
+
+    @FXML
+    private void backImage() {
+        _index = (_index - 1 + _images.size()) % _images.size();
+        imageTable.setImage(_images.get(_index));
+    }
+
+    
 
     @FXML
     public void jumpToPlayers(){
