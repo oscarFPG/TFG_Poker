785bdc088e5ed8ad19bf7d278a489aae6235e0eb
CarlaTTaipe25 <carlatoa@ucm.es>
2026-04-12 22:44:24 +0200

    Update InGameWindowController.java

--- tfgpoker/client/src/main/java/com/ucm/client/views/original/controllers/InGameWindowController.java
+++ tfgpoker/client/src/main/java/com/ucm/client/views/original/controllers/InGameWindowController.java
@@ -26,6 +26,8 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 
