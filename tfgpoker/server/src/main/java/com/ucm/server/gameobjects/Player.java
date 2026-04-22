bd12e09ba6bde68e3e1255accf68a70990de391a
CarlaTTaipe25 <carlatoa@ucm.es>
2026-04-22 19:17:49 +0200

    error al cerrar ventanas cliente

--- tfgpoker/server/src/main/java/com/ucm/server/gameobjects/Player.java
+++ tfgpoker/server/src/main/java/com/ucm/server/gameobjects/Player.java
@@ -190,16 +190,14 @@ public class Player implements IPlayerActions {
 
     /* Player methods */
     public String makePlay(int sb, int bb, int maxBet) throws TurnTimeoutException {
-        
-        String action = null;
+
         try {
-            action = _playerInfo.notifyMakePlay(sb, bb, maxBet, this);
+           return _playerInfo.notifyMakePlay(sb, bb, maxBet, this);
         }
         catch (IOException e) {
-            e.printStackTrace();
+           throw new TurnTimeoutException();
         }
-        
-        return action;
+
     }
 
     public int placeOnBetMoney() {
