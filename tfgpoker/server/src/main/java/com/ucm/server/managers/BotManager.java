1c7c0971c04122fa10343d316c8100d46a32be61
CarlaTTaipe25 <carlatoa@ucm.es>
2026-07-09 16:15:32 +0200

    Automatizacion de los bots hecho

--- tfgpoker/server/src/main/java/com/ucm/server/managers/BotManager.java
+++ tfgpoker/server/src/main/java/com/ucm/server/managers/BotManager.java
@@ -1,8 +1,10 @@
 package com.ucm.server.managers;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Supplier;
 
+import com.ucm.common.GameType;
 import com.ucm.server.gameobjects.Bot;
 import com.ucm.server.players.AgentCFR;
 import com.ucm.server.players.GeminiLLM;
@@ -10,22 +12,27 @@ import com.ucm.server.players.LlamaPokerLLM;
 
 
 public class BotManager {
-    
-    private static final List<Bot>AVAILABLE_BOTS  = Arrays.asList(
-        new GeminiLLM(),
-        new LlamaPokerLLM(),
-        new AgentCFR()
-    );
 
+    private static final Map<Integer, Supplier<Bot>> BOTS = new HashMap<>();
 
-    public static Bot createBot(int ID) {
+    private BotManager(){}
 
-        for(Bot bot : AVAILABLE_BOTS) {
-            if(bot.getIdBot() == ID)
-                return bot;
+    static {
+        register(GameType.BOT_GEMINI, GeminiLLM::new);
+        register(GameType.BOT_LLAMA, LlamaPokerLLM::new);
+        register(GameType.BOT_NN_MODEL_1, AgentCFR::new);
+    }
+
+    private static void register(int botId, Supplier<Bot> factory){
+        BOTS.put(botId, factory);
+    }
+
+    public static Bot createBot(int botId){
+        Supplier<Bot> factory = BOTS.get(botId);
+        if(factory == null){
+            throw new IllegalArgumentException("Unknown bot id:" + botId);
         }
-
-        return null;
+        return factory.get();
     }
 
 }
