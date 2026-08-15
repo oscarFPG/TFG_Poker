bdde06312015afb154e49d28b2da5ab4096cf485
CarlaTTaipe25 <carlatoa@ucm.es>
2026-08-15 12:24:19 +0200

    Revert "continuando con la vision de los estilos"
    This reverts commit 43589aefb4505cf6ba96b1da24fa9e889bdeeedc.

--- tfgpoker/client/src/main/java/com/ucm/client/AvatarGenerator.java
+++ tfgpoker/client/src/main/java/com/ucm/client/AvatarGenerator.java
@@ -20,40 +20,27 @@ public class AvatarGenerator {
 
     public static Image generateBot(String name, int size) {
 
-        String type = getTypeBot(name);
         BotStyle style = BotStyle.createByName(name);
+        String type;
+
+        if(name.startsWith("GeminiLLM")){
+            type = GameType.GEMINI_NAME;
+        }
+        else if (){
+            
+        }
         
-        return generateBotAvatar(size, generateBotColor(type), style);
-    }
-
-    private static String getTypeBot(String name){
-        if (name.startsWith("GeminiLLM")){
-            return GameType.GEMINI_NAME;
-        }
-        else if (name.startsWith("LlamaPokerLLM")){
-            return GameType.LLAMA_NAME;
-        }
-        else if (name.startsWith("DeepCFR")){
-           return GameType.NN_MODEL_NAME;
-        }
-        else if (name.startsWith("FSM")){
-           return GameType.FSM_NAME;
-        }
-        else{
-            return GameType.DEFAULT_NAME;
+        switch (name) {
+            case GEMINI_BOT:
+                return generateBotAvatar("GEMINI_BOT", size, Color.web("#7B61FF"));
+            case LLAMA_BOT:
+                return generateBotAvatar("LLAMA_BOT", size, Color.web("#1417dd"));
+            default:
+                throw new IllegalArgumentException("Unsupported avatar type");
         }
     }
 
-    private static Color generateBotColor(String botType){
-        
-        int hash = Math.abs(botType.hashCode());
-
-        double hue = hash % 360;
-
-        return Color.hsb(hue, 0.7, 0.9);
-    }
-
-    private static Image generateBotAvatar(int size, Color mainColor, BotStyle style) {
+    private static Image generateBotAvatar(String seed, int size, Color mainColor) {
 
         Canvas canvas = new Canvas(size, size);
         GraphicsContext gc = canvas.getGraphicsContext2D();
@@ -66,75 +53,27 @@ public class AvatarGenerator {
         gc.fillRect(size * 0.1, size * 0.3, size * 0.8, size * 0.25);
 
         // Ojos LED
-        generateEyesByStyle(gc, size, style);
-        
-        // Boca 
-        gc.setStroke(Color.BLACK);
-        gc.setLineWidth(size * 0.03);
-        generateMouthByStyle(gc, size, style);
+        gc.setFill(Color.CYAN);
+        double eyeSize = size * 0.1;
+        gc.fillRect(size * 0.3, size * 0.37, eyeSize, eyeSize);
+        gc.fillRect(size * 0.6, size * 0.37, eyeSize, eyeSize);
 
         
+        // Boca RECTA
+        gc.setStroke(Color.BLACK);
+        gc.setLineWidth(size * 0.03);
+        gc.strokeLine(
+            size * 0.35,
+            size * 0.65,
+            size * 0.65,
+            size * 0.65
+        );
+
+
+
         WritableImage image = new WritableImage(size, size);
         canvas.snapshot(null, image);
         return image;
-    }
-
-    private static void generateEyesByStyle(GraphicsContext gc, int size, BotStyle style){
-        switch (style) {
-            case DEFAULT -> {
-                gc.setFill(Color.CYAN);
-                gc.fillOval(size * 0.28, size * 0.33, size * 0.10, size * 0.10);
-
-            }
-            case TIGHT_PASSIVE -> {
-                gc.setStroke(Color.CYAN);
-                gc.strokeLine(size * 0.28, size * 0.38, size * 0.38, size * 0.38);
-            }
-            case TIGHT_AGGRESSIVE -> {
-                gc.setStroke(Color.ORANGE);
-                gc.strokeLine(size * 0.28, size * 0.42, size * 0.38, size * 0.35);
-            }
-            case LOOSE_PASSIVE -> {
-                gc.setFill(Color.LIMEGREEN);
-                gc.fillOval(size * 0.28, size * 0.32, size * 0.12, size * 0.12);
-            }
-            case LOOSE_AGGRESSIVE -> {
-                gc.setFill(Color.YELLOW);
-                gc.fillOval(size * 0.26, size * 0.30, size * 0.14, size * 0.14);
-            }
-            case MANIAC -> {
-                gc.setFill(Color.RED);
-                gc.fillOval(size * 0.24, size * 0.28, size * 0.16, size * 0.16);
-            }
-        }
-    }
-
-    private static void  generateMouthByStyle(GraphicsContext gc, int size, BotStyle style){
-        switch (style) {
-            case DEFAULT -> {
-                gc.strokeLine(size * 0.35, size * 0.65, size * 0.65, size * 0.65);
-            }
-            case TIGHT_PASSIVE -> {
-                gc.strokeLine(size * 0.38, size * 0.67, size * 0.62, size * 0.67);
-            }
-            case TIGHT_AGGRESSIVE -> {
-                gc.strokeLine(size * 0.35, size * 0.68, size * 0.65, size * 0.62);
-            }
-            case LOOSE_PASSIVE -> {
-                gc.strokeArc(size * 0.35, size * 0.58, size * 0.30, size * 0.15, 180, 180, javafx.scene.shape.ArcType.OPEN);
-            }
-            case LOOSE_AGGRESSIVE -> {
-                gc.strokeArc(size * 0.30, size * 0.55, size * 0.40, size * 0.20, 180, 180, javafx.scene.shape.ArcType.OPEN);
-            }
-            case MANIAC -> {
-                gc.setStroke(Color.RED);
-                gc.strokeArc(size * 0.28, size * 0.54, size * 0.44, size * 0.22, 180, 180, javafx.scene.shape.ArcType.OPEN);
-                for(int i = 0; i < 5; i++) {
-                    double x = size * 0.35 + i * size * 0.07;
-                    gc.strokeLine(x, size * 0.64, x, size * 0.69);
-                }
-            }
-        }
     }
     
     private static Image generate(String seed, int size) {
